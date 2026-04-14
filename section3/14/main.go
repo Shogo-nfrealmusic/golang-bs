d7abc8b254cbba156451ffb69b6a193c57eda999
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-04-15 01:14:52 +0900

    array

---
+++ section3/14/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [2]int
+	a[0] = 100
+	a[1] = 200
+	fmt.Println(a[0])
+	fmt.Println(a[1])
+
+	var b [2]int = [2]int{100, 200}
+	fmt.Println(b)
+
+	var c []int = []int{100, 200}
+	c = append(c, 300)
+	fmt.Println(c)
+}
