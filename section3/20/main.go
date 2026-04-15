7d7b43305027797d1895a851944d4c908075cc15
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-04-16 01:56:32 +0900

    clogure

---
+++ section3/20/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func incrementGenerator() (func() int) {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
+
+func circleArea(pi float64) func(radius float64) float64 {
+	return func(radius float64) float64 {
+		return pi * radius * radius
+	}
+}
+
+func main() {
+	counter := incrementGenerator()
+	fmt.Println(counter())
+	fmt.Println(counter())
+}
