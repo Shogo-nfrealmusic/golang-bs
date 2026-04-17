c942681579871cae02b3e6b5c3fe8318a88ba83f
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-04-17 18:30:50 +0900

    error handling

---
+++ section4/30/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("main.go")
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	defer file.Close()
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	fmt.Println(count, string(data))
+	fmt.Println(string(data))
+
+	if err = os.Chdir("test"); err != nil {
+		log.Fatalln("Error")
+	}
+	fmt.Println("Success")
+}
