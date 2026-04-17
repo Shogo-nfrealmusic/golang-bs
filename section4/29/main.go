8fef6eff5262a926f984383ec5e37e728317b777
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-04-17 18:23:40 +0900

    log

---
+++ section4/29/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func LoggingSettings(logFile string) {
+	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
+}
+
+func main() {
+	LoggingSettings("test.log")
+	log.Println("logging")
+	log.Fatalln("error")
+}
