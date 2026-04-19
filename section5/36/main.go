ef0aef45f2b190c7fb57bdab3af7c9498e06484a
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-04-19 14:03:35 +0900

    struct

--- section5/36/main.go
+++ section5/36/main.go
@@ -8,7 +8,23 @@ type Vertex struct {
 	s string
 }
 
+func changeVertex(v Vertex) {
+	v.x = 1000
+}
+
+func changeVertex2(v *Vertex) {
+	v.x = 1000
+}
+
 func main() {
+	v := Vertex{x: 1, y: 2, s: "test"}
+	changeVertex(v)
+	fmt.Println(v)
+
+	v2 := &Vertex{x: 1, y: 2, s: "test"}
+	changeVertex2(v2)
+	fmt.Println(*v2)
+	/*
 	v := Vertex{x: 1, y: 2}
 	fmt.Println(v)
 
@@ -17,4 +33,12 @@ func main() {
 
 	v3 := Vertex{1, 2, "test"}
 	fmt.Println(v3)
+
+	var v5 Vertex
+	fmt.Println(v5)
+
+	v6 := new(Vertex)
+	fmt.Printf("%T\n", v6)
+	*/
+	
 }
