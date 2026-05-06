fe6d386f6e61e1f40576f4af76a200ffb89191a8
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-05-06 18:52:04 +0900

    rsi

--- section11/78/main.go
+++ section11/78/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
+)
+
 func main() {
-	
+	spy, _ := quote.NewQuoteFromTiingo(
+		"SPY",
+		"2016-01-01",
+		"2016-04-01",
+		quote.Daily,
+		os.Getenv("TIINGO_TOKEN"),
+	)
+	fmt.Println(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
 }
