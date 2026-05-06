ed03d605cf56b7f08f45a09278784722793e94ab
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-05-06 19:10:17 +0900

    websocket#

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/brianolson/cbor_go v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/markcheno/go-quote v0.0.0-20251022180205-ebbbbdb8e2b0 // indirect
 	github.com/markcheno/go-talib v0.0.0-20250114000313-ec55a20c902f // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- section11/80/main.go
+++ section11/80/main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"log"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+type JsonRPC2 struct {
+	Version string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	Result  interface{} `json:"result,omitempty"`
+	ID      *int        `json:"id,omitempty"`
+}
+
+type SubscribeParams struct {
+	Channel string `json:"channel"`
+}
+
 func main() {
-	
+	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path: "/json-rpc"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	subscribe := &JsonRPC2{
+		Version: "2.0",
+		Method:  "subscribe",
+		Params:  &SubscribeParams{Channel: "lightning_ticker_BTC_JPY"},
+	}
+	if err := c.WriteJSON(subscribe); err != nil {
+		log.Fatal("subscribe:", err)
+	}
+
+	for {
+		message := new(JsonRPC2)
+		if err := c.ReadJSON(message); err != nil {
+			log.Println("read:", err)
+			return
+		}
+		if message.Method == "channelMessage" {
+			log.Println(message.Params)
+		}
+	}
 }
