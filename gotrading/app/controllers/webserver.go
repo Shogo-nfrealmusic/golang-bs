1a4caaa26d75be20b757d94f1796f27508d2e0a5
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-05-24 16:36:56 +0900

    data frame api

--- gotrading/app/controllers/webserver.go
+++ gotrading/app/controllers/webserver.go
@@ -6,7 +6,10 @@ import (
 	"gotrading/app/models"
 	"gotrading/config"
 	"html/template"
+	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 var templates = template.Must(template.ParseFiles("app/views/google.html"))
@@ -59,7 +62,79 @@ func viewChartHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type JSONEncoder struct {
+	Error string `json:"error"`
+	Code int `json:"code"`
+}
+
+func APIError(w http.ResponseWriter, err error, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	jsonError, marshalErr := json.Marshal(JSONEncoder{Error: err.Error(), Code: code})
+	if marshalErr != nil {
+		log.Fatal(marshalErr)
+	}
+	w.Write(jsonError)
+}
+
+var apiVaidPaths = regexp.MustCompile(`^/api/candle/$`)
+
+func apiMakeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := apiVaidPaths.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			APIError(w, fmt.Errorf("invalid path: %s", r.URL.Path), http.StatusNotFound)
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
+	productCode := r.URL.Query().Get("product_code")
+	if productCode == "" {
+		APIError(w, fmt.Errorf("product_code is required"), http.StatusBadRequest)
+		return
+	}
+
+	limit := 100
+	if strLimit := r.URL.Query().Get("limit"); strLimit != "" {
+		n, err := strconv.Atoi(strLimit)
+		if err != nil || n <= 0 || n > 1000 {
+			APIError(w, fmt.Errorf("limit must be between 1 and 1000"), http.StatusBadRequest)
+			return
+		}
+		limit = n
+	}
+
+	duration := r.URL.Query().Get("duration")
+	if duration == "" {
+		duration = "1m"
+	}
+	durationTime, ok := config.Config.Durations[duration]
+	if !ok {
+		APIError(w, fmt.Errorf("invalid duration: %s", duration), http.StatusBadRequest)
+		return
+	}
+
+	df, err := models.GetAllCandles(productCode, durationTime, limit)
+	if err != nil {
+		APIError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	js, err := json.Marshal(df)
+	if err != nil {
+		APIError(w, err, http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+
 func StartWebServer() error {
+	http.HandleFunc("/api/candle/", apiMakeHandler(apiCandleHandler))
 	http.HandleFunc("/chart", viewChartHandler)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
