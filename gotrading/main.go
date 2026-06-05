38187ed5fe8557e5b44792b56378c3f3a88ed857
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-06-05 19:06:56 +0900

    backtest

--- gotrading/main.go
+++ gotrading/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"gotrading/app/controllers"
-	"gotrading/config"
-	"gotrading/utils"
+	"gotrading/app/models"
+	"time"
 )
 
+// import (
+// 	"gotrading/app/controllers"
+// 	"gotrading/config"
+// 	"gotrading/utils"
+// )
+
+// func main() {
+// 	utils.LoggingSettings(config.Config.LogFile)
+// 	go controllers.StreamINgestionData()
+// 	if err := controllers.StartWebServer(); err != nil {
+// 		panic(err)
+// 	}
+// }
+
 func main() {
-	utils.LoggingSettings(config.Config.LogFile)
-	go controllers.StreamINgestionData()
-	if err := controllers.StartWebServer(); err != nil {
-		panic(err)
-	}
+	s := models.NewSignalEvents()
+	df, _ := models.GetAllCandles("BTC_JPY", time.Minute, 10)
+	c1 := df.Candles[0]
+	c2 := df.Candles[5]
+	s.Buy("BTC_JPY", c1.Time, c1.Close, 1.0, true)
+	s.Sell("BTC_JPY", c2.Time, c2.Close, 1.0, true)
 }
